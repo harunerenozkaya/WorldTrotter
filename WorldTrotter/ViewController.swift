d7b5152c58861a5bdd9066659e52319264fd19ac
Harun Eren Özkaya <harunerenozkaya@gmail.com>
2023-01-26 00:51:09 +0300

    style has been upgraded

--- WorldTrotter/ViewController.swift
+++ WorldTrotter/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var value : UILabel!
+    
+    @IBAction func converterChanged(textField:UITextField){
+        value.text = textField.text
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        value.text = "0"
     }
 
-
+    
 }
 
